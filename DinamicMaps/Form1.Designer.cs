b55c516999eabe3c0a9b01d3e31fe0f9c2407834
Beril Bolukbasi <beril.bls23107@gmail.com>
2025-09-21 14:03:33 +0300

    added markers click

--- DinamicMaps/Form1.Designer.cs
+++ DinamicMaps/Form1.Designer.cs
@@ -37,6 +37,14 @@ namespace DinamicMaps
             this.textBoxEnlem = new System.Windows.Forms.TextBox();
             this.textBoxBoylam = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // map
@@ -65,14 +73,15 @@ namespace DinamicMaps
             this.map.Size = new System.Drawing.Size(800, 718);
             this.map.TabIndex = 0;
             this.map.Zoom = 0D;
+            this.map.OnMarkerClick += new GMap.NET.WindowsForms.MarkerClick(this.map_OnMarkerClick);
             this.map.Load += new System.EventHandler(this.gMapControl1_Load);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(874, 471);
+            this.button1.Location = new System.Drawing.Point(874, 475);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(251, 85);
+            this.button1.Size = new System.Drawing.Size(251, 54);
             this.button1.TabIndex = 1;
             this.button1.Text = "Haritada Göster";
             this.button1.UseVisualStyleBackColor = true;
@@ -120,11 +129,90 @@ namespace DinamicMaps
             this.label3.TabIndex = 6;
             this.label3.Text = "ondalık girerken virgül kullanın ";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1230, 313);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Enlem:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1306, 310);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1230, 370);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Boylam:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1306, 370);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(177, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1269, 421);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(189, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "ondalık girerken virgül kullanın ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1233, 475);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(251, 54);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Haritada Göster";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1201, 274);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Marker2";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(846, 274);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 16);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Marker1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1916, 742);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBoxBoylam);
             this.Controls.Add(this.textBoxEnlem);
@@ -161,6 +249,14 @@ namespace DinamicMaps
         private System.Windows.Forms.TextBox textBoxEnlem;
         private System.Windows.Forms.TextBox textBoxBoylam;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- DinamicMaps/Form1.cs
+++ DinamicMaps/Form1.cs
@@ -25,8 +25,8 @@ namespace DinamicMaps
             map.Zoom = 4;
             map.MinZoom = 1;
             map.MaxZoom = 30;
-            overlay1 = new GMapOverlay();                           //bir katman oluşturuldu.
-            map.Overlays.Add(overlay1);                             //katman haritaya eklendi. (önce katman daha sonra marker eklenir)
+            overlay1 = new GMapOverlay();                                       //bir katman oluşturuldu.
+            map.Overlays.Add(overlay1);                                         //katman haritaya eklendi. (önce katman daha sonra marker eklenir)
 
 
         }
@@ -36,7 +36,14 @@ namespace DinamicMaps
             PointLatLng location1 = new PointLatLng(Convert.ToDouble(textBoxEnlem.Text), 
                                                     Convert.ToDouble(textBoxBoylam.Text));
             GMarkerGoogle marker = new GMarkerGoogle(location1, GMarkerGoogleType.lightblue_dot);
-            overlay1.Markers.Add(marker);                           //marker katmana eklendi.
+
+            marker.ToolTipText = "\n Location \n Car  \n From: Ankara \n To: İstanbul";
+            marker.ToolTip.Fill = System.Drawing.Brushes.LightGray;   
+            marker.ToolTip.Foreground = System.Drawing.Brushes.Black;
+            marker.ToolTipMode = MarkerTooltipMode.OnMouseOver;                 //marker üzerine gelince tooltip göster.
+            marker.Tag = 101;                                                   //marker a id atandı.
+
+            overlay1.Markers.Add(marker);                                       //marker katmana eklendi.
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
@@ -48,6 +55,27 @@ namespace DinamicMaps
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PointLatLng location2 = new PointLatLng(Convert.ToDouble(textBox2.Text),
+                                                    Convert.ToDouble(textBox1.Text));
+            GMarkerGoogle marker2 = new GMarkerGoogle(location2, GMarkerGoogleType.red_dot);
+            marker2.Tag = 102;
+
+            overlay1.Markers.Add(marker2);
+        }
+
+        private void map_OnMarkerClick(GMapMarker item, MouseEventArgs e)
+        {
+            int markerId = (int)item.Tag;
+            Console.WriteLine("Marker ID: " + markerId + "click the marker");
         }
     }
 }
